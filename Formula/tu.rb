fc22637297dd99400818af1b713e4da59537e056
Sahil Ahuja <sahilahuja@gmail.com>
2026-04-01 14:52:54 +0530

    tu 0.4.7

--- Formula/tu.rb
+++ Formula/tu.rb
@@ -5,7 +5,7 @@ class Tu < Formula
   desc "AI coding assistant cost tracking CLI"
   homepage "https://github.com/sahil87/tu"
   url "https://github.com/sahil87/tu.git",
-      using: :git, tag: "v0.4.6"
+      using: :git, tag: "v0.4.7"
   license "MIT"
 
   env :std
